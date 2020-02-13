6560e75565f97e770f38919e459bfbab80782330
Mateusz Kozakiewicz <mateusz.kozakiewicz@boldare.com>
2020-02-13 23:35:30 +0100

    change version number

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 4000;
 
 app.use(cors());
-app.get("/version", (req, res) => res.json({ version: "0.1" }));
+app.get("/version", (req, res) => res.json({ version: "0.1.2" }));
 app.get("/", (req, res) => res.sendStatus(200));
 
 app.listen(port, () => console.log(`Application listening on port ${port}`));
