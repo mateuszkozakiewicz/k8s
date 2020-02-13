a677bb39fbfe39d76b37df21fa65916ef04a2cd0
Mateusz Kozakiewicz <mateusz.kozakiewicz@boldare.com>
2020-02-13 15:46:16 +0100

    add backend

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 4000;
+
+app.use(cors());
+app.get("/version", (req, res) => res.json({ version: "0.1" }));
+app.get("/", (req, res) => res.sendStatus(200));
+
+app.listen(port, () => console.log(`Application listening on port ${port}`));
